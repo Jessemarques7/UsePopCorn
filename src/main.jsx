f8cf0f6a6270ae656c635434dff653199d3335ac
Jesse Marques <jessemateus919@gmail.com>
2024-04-14 14:16:41 -0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./assets/components/StarRating";
-// import App from './App.jsx'
-// import './index.css'
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating color={"red"} size={24} defaultRating={1} />
+    <StarRating color={"red"} size={24} defaultRating={1} /> */}
   </React.StrictMode>
 );
