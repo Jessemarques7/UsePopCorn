e23f1fb6954af13c9390cbec028bc61f6722c48a
Jesse Marques <jessemateus919@gmail.com>
2024-04-21 14:08:50 -0300

    refs

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,22 @@ import WhatchedMovieList from "./assets/components/WhatchedMovieList";
 export const KEY = "8e6b0cc4";
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+
+  // const [watched, setWatched] = useState(function () {
+  //   const storedValue = localStorage.getItem("watched");
+  //   return JSON.parse(storedValue);
+  // });
+
+  const [watched, setWatched] = useState(() =>
+    JSON.parse(localStorage.getItem("watched"))
+  );
 
   function handleSelectedMovie(id) {
     setSelectedId((selectedId) => (selectedId === id ? null : id));
@@ -30,11 +40,20 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
--- src/assets/components/MovieDetails.jsx
+++ src/assets/components/MovieDetails.jsx
@@ -140,7 +140,6 @@ export default function MovieDetails({
                 </p>
               )}
             </div>
-
             <p>
               <em>{plot}</em>
             </p>
